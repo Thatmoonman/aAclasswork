7a26c7962e9db7598493b86a2498457712703d3e
App Academy Student <appacademystudent@Apps-MacBook-Air.local>
2022-06-27 20:44:19 -0400

    endofclass

--- w1d1/test.js
+++ w1d1/test.js
@@ -1,5 +1,20 @@
-let name = "something"
+let name = "Justin"
+console.log(name[3])
+console.log(name.length)
+console.log(name[name.length-1])
+console.log(name.indexOf("u"))
+console.log(name.indexOf("p"))
+console.log(name.concat(" Kilburn"))
+console.log(name)
+name = name.concat(" Kilburn")
 console.log(name)
 // anything at all
-let anything
-console.log(anything)
+console.log(10+3)
+console.log(10-3)
+console.log(10*3)
+console.log(10/3)
+console.log(10%3)
+console.log(10%2)
+console.log(2%5)
+console.log(true||false&&false)
+console.log(true&&false)
