14181b6b6aa2939dd77aaaecd31136a40293cc14
App Academy Student <appacademystudent@Apps-Air.lan>
2022-06-27 22:58:11 -0400

    w1d1 homeworkpart3+4

--- w1d1/homework.js
+++ w1d1/homework.js
@@ -1,4 +1,4 @@
-// Part 1
+console.log("Part 1")
 console.log(4 + 4 / 2)
 // 6
 console.log((2 + -7) * 3)
@@ -63,7 +63,7 @@ console.log('bootcamp'.indexOf('o') > -1)
 // true
 console.log('science'.indexOf('x') === -1)
 // true
-// Part 2
+console.log("Part 2")
 // 1
 var idx = 'abcde'.indexOf('D');
 idx = idx + 11;
@@ -88,4 +88,81 @@ var sentence = 'welcome to bootcamp prep';
 var lastChar = sentence[sentence.length - 1];
 console.log(lastChar); // p
 console.log(sentence.indexOf(lastChar)); // 18 **first instance only**
-// Part 3
+console.log("Part 3")
+// 5
+var age = 20; // try different numbers here
+
+if (age > 30) {
+  console.log('older than 30');
+} else {
+  console.log('younger than 30');
+}
+// younger than 30
+// 6
+var str = 'popcorn'; // try different strings here
+
+if (str.length > 10) {
+  console.log('long string');
+} else {
+  console.log('short string');
+}
+if (str[0] === 'p') {
+  console.log('starts with p');
+}
+// short string
+// starts with p
+// 7
+var num = 52; // try different numbers here
+
+if (num % 3 === 0) {
+  console.log('divisible by 3');
+} else if (num % 5 === 0) {
+  console.log('divisible by 5');
+}
+// undefined **logs nothing**
+var num = 30; // try different numbers here
+
+if (num % 3 === 0) {
+  console.log('divisible by 3');
+} else if (num % 5 === 0) {
+  console.log('divisible by 5');
+}
+// divisible by 3
+// 8
+var num = 30; // try different numbers here
+
+if (num % 3 === 0) {
+  console.log('divisible by 3');
+}
+if (num % 5 === 0) {
+  console.log('divisible by 5');
+}
+// divisible by 3
+// divisible by 5
+// 9
+var str = 'here we go!'; // try different strings here
+
+if (str[0] === str[0].toUpperCase()) {
+  console.log('starts with a capital!');
+}
+if (str[str.length - 1] === str[str.length - 1].toUpperCase()) {
+  console.log('ends with a capital!');
+}
+// undefined **'!' is a capital!!!**
+// 10
+var num = 101; // try different numbers here
+
+if (num > 0) {
+  console.log('positive');
+} else if (num < 0) {
+  console.log('negative');
+} else {
+  console.log(0);
+}
+if (num % 2 === 0) {
+  console.log('even');
+} else {
+  console.log('odd');
+}
+// positive
+// odd
---
+++ w1d1/typingexc.js
@@ -0,0 +1,24 @@
+//type each 10x
+// #1
+var num = 11; // feel free to change the value of this variable
+if (num > 5) {
+  console.log('if');
+}
+
+// #2
+var num = 5; // feel free to change the value of this variable
+if (num > 5) {
+  console.log('if');
+} else {
+  console.log('else');
+}
+
+// #3
+var num = 0; // feel free to change the value of this variable
+if (num < 0) {
+  console.log('if');
+} else if (num > 0) {
+  console.log('else if');
+} else {
+  console.log('else');
+}
