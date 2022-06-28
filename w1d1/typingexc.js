53f68d871227cabd8b3885551d2a896dc9a928b7
App Academy Student <appacademystudent@Apps-Air.lan>
2022-06-28 16:31:36 -0400

    d1hw

--- w1d1/typingexc.js
+++ w1d1/typingexc.js
@@ -1,24 +1,66 @@
 //type each 10x
 // #1
-var num = 11; // feel free to change the value of this variable
+let num = 11; // feel free to change the value of this variable
 if (num > 5) {
   console.log('if');
 }
-
+let numA = 22;
+if (numA > 5) {
+  console.log('if A')
+}
+let numB = 1;
+if (numB > 5) {
+  console.log('if B')
+}
+let numC = "notanumber"
+if (numC > 5) {
+  console.log('if C')
+}
+let numD = 3 + 3
+if (numD > 5) {
+  console.log('if D')
+}
+let numE = 5
+if (numE > 5) {
+  console.log('if E')
+}
 // #2
-var num = 5; // feel free to change the value of this variable
 if (num > 5) {
   console.log('if');
 } else {
   console.log('else');
 }
-
-// #3
-var num = 0; // feel free to change the value of this variable
-if (num < 0) {
-  console.log('if');
-} else if (num > 0) {
-  console.log('else if');
+if (numA > 5) {
+  console.log('if A');
 } else {
-  console.log('else');
+  console.log('else A');
+}
+if (numB > 5) {
+  console.log('if B');
+} else {
+  console.log('else B')
+}
+if (numC > 5) {
+  console.log('if C')
+} else {
+  console.log('else C')
+}
+if (numD > 5) {
+  console.log('if D')
+} else {
+  console.log('else D')
+}
+if (numE > 5) {
+  console.log('if E')
+} else {
+  console.log('esle E')
+}
+// #3
+var numF = 0; // feel free to change the value of this variable
+if (numF < 0) {
+  console.log('if F');
+} else if (numF > 0) {
+  console.log('else if F');
+} else {
+  console.log('else F');
 }
