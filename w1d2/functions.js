0136fd38f0c064dfbf3f1bac6395e40724edeca4
App Academy Student <email@email.com>
2022-06-28 20:07:32 -0400

    push

--- w1d2/functions.js
+++ w1d2/functions.js
@@ -147,11 +147,28 @@ let average = (num1, num2) => {
 // }
 // isEven(5)
 // isEven(6)
-function isOdd(num) {
-  return num % 2 !== 0
-}
-console.log(isEven(5))
+// function isOdd(num) {
+//   return num % 2 !== 0
+// }
+// console.log(isEven(5))
 
-function isEven(num) {
-  return !isOdd(num)
+// function isEven(num) {
+//   return !isOdd(num)
+// }
+// Write a function `averageOfFour(num1, num2, num3, num4)` that takes in four
+// numbers. The function should return the average of all of the numbers.
+//
+// Examples:
+//
+// averageOfFour(10, 10, 15, 5); // => 10
+// averageOfFour(3, 10, 11, 4); // => 7
+// averageOfFour(1, 2, 3, 4); // => 2.5
+function averageOfFour(num1,num2,num3,num4) {
+  let avg = sum(num1,num2,num3,num4)/4
+  return avg
 }
+//return variable, console log function
+function sum(num1,num2,num3,num4) {
+return num1+num2+num3+num4
+}
+console.log(averageOfFour(1,2,3,4))
