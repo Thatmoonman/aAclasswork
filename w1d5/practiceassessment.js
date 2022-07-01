e4f5de2e86f4821febb204250d468286bbd217ff
Kimi Mongello <yourfriendkimi@Kimis-MBP.lan>
2022-07-01 10:36:30 -0400

    prac assess pt1

---
+++ w1d5/practiceassessment.js
@@ -0,0 +1,116 @@
+//Problem 1
+// Write a function `isInside(array, ele)` that returns true if `ele` is an element
+// inside of the `array`, false otherwise.
+//
+// HINT: use indexOf
+//
+function isInside(array, ele) {
+    return array.indexOf(ele) !== -1
+}
+//console.log(isInside(['chase', 'phi', 'eliot'], 'eliot')); // => true
+//console.log(isInside(['chase', 'phi', 'eliot'], 'fred')); // => false
+
+//Problem 2
+// Write a function `reverseStr(str)` that takes in a string as a parameter and
+// returns the string but with the characters in reverse.
+//
+// Examples:
+//
+function reverseStr(str) {
+    let reverse = str.split("")
+    let reverseWord = []
+    for (i=reverse.length; i>=0; i--) {
+        reverseWord.push(reverse[i])
+    }
+    return reverseWord.join("")
+}
+//console.log(reverseStr('bootcamp')); //=> 'pmactoob'
+//console.log(reverseStr('App Academy')); //=> 'ymedacA ppA'
+
+//Problem 3
+// Write a function `luckySevens(max)` that returns an array of all numbers up
+// to max (inclusive) that are divisible by 7.
+//
+// Examples:
+//
+function luckySevens(max) {
+    let sevens = []
+    for (i=1; i<=max; i++) {
+        if (i % 7 === 0) {
+            sevens.push(i)
+        }
+    }
+    return sevens
+}
+//console.log(luckySevens(25)); // => [ 7, 14, 21 ]
+//console.log(luckySevens(42)); // => [ 7, 14, 21, 28, 35, 42 ]
+
+//Problem 4
+// Write a function `copyMachine(element, num)` that takes in an element and a number
+// it should return an array of length `num` that is filled with `element`.
+//
+// Examples:
+//
+function copyMachine(element, num) {
+    let copy = []
+    for (i=0; i<num; i++) {
+    copy.push(element)
+    }
+    return copy
+}
+//console.log(copyMachine('candy', 0)); // => []
+//console.log(copyMachine('candy', 2)); // => [ 'candy', 'candy' ]
+//console.log(copyMachine('bread', 4)); // => [ 'bread', 'bread', 'bread', 'bread' ]
+//console.log(copyMachine(11, 6)); // => [ 11, 11, 11, 11, 11, 11 ]
+
+//Problem 5
+// Write a function `everyOtherWord(sentence)` that takes in a sentence and returns
+// an array containing every other word in that sentence.
+//
+// Examples:
+//
+function everyOtherWord(sentence) {
+    let everyOther = []
+    let words = sentence.split(" ")
+    for (i=0; i<words.length; i+=2 ) {
+        everyOther.push(words[i])
+    }
+    return everyOther
+}
+//console.log(everyOtherWord('hello how are you doing on this lovely day?')); // => [ 'hello', 'are', 'doing', 'this', 'day?' ]
+//console.log(everyOtherWord('the weather is wonderful')); // => [ 'the', 'is' ]
+
+//Problem 6
+// Write a function `wordYeller(sentence)` that takes in a sentence string as
+// input. It should return the sentence where every word has an exclamation
+// point behind it, except words that already have punctuation. Punctuation
+// marks are ". , ! ? ; :"
+//
+// Example 1
+// var sent = "Stop it now! Please, wont you stop?";
+// var yelledWords = wordYeller(sent);
+// yelledWords === "Stop! it! now! Please, wont! you! stop?"; // => true
+//
+// Example 2
+// var words = "Go to the store and grab the following: milk, bread, run, and cake";
+// wordYeller(words) === "Go! to! the! store! and! grab! the! following: milk, bread, run, and! cake!"; // => true
+function wordYeller(sent) {
+    let words = sent.split("")
+    let marks = [",.!?;:"]
+    let yell = []
+    for (i=0; i<words.length; i++) {
+        if (marks.indexOf(words[i]) !== -1) {
+        yell.push(words[i] + " ")
+        i++
+        } else {
+            if (words[i].indexOf(" ") !== -1) {
+                yell.push("!" + words[i])
+            }
+        }
+        yell.push(words[i])
+    }
+    return yell.join("")
+}
+
+console.log(wordYeller("Stop it now! Please, wont you stop?"));
+console.log(wordYeller("Go to the store and grab the following: milk, bread, run, and cake"));
