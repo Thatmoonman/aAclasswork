c1567c5f48bb9f398f8f6c57f72de34a67a4d6ba
Justin Kilburn <thatmoonman@gmail.com>
2022-07-06 18:56:57 -0400

    zerosums

--- w2d3/notes.js
+++ w2d3/notes.js
@@ -22,14 +22,59 @@ function testObj() {
 
 // console.log([0,1,2,"something"][3])
 
-let multiArray = [
-    ["a","b","c"],
-    ["1","2","3"],
-    ["g","h"]
-]
+// ###################################################
+// let multiArray = [
+//     ["a","b","c"],
+//     ["1","2","3"],
+//     ["g","h"]
+// ]
 
-for (let i =0; i<multiArray.length; i++) {
-    let innerArray = multiArray[i]
-    for (let j=0; j < innerArray.length; j++)
-    console.log(innerArray[j])
+// for (let i =0; i<multiArray.length; i++) {
+//     let innerArray = multiArray[i]
+
+//     for (let j=0; j < innerArray.length; j++)
+//         console.log(innerArray[j])
+// }
+
+// ###################################################
+
+// let names = ["Wei","Jimmy","Justin"]
+// printNames(names)
+
+function printNames(names) {
+
+    for (let i=0;i<names.length;i++) {
+        for (let j=0;j<names.length;j++) {
+            console.log(names[i],names[j])
+        }
+    }
 }
+
+// ###################################################
+// let names = ["Wei","Jimmy","Justin","Jonathon"]
+// printUniques(names)
+
+function printUniques(names) {
+
+    for (let i=0;i<names.length;i++) {
+        for (let j=i+1;j<names.length;j++) {
+            console.log([names[i],names[j]])
+        }
+    }
+}
+// ###################################################
+let numbers = [0,2,5,-2,7]
+pairZeroSum(numbers)
+
+function pairZeroSum(numbers) {
+    for (let i=0; i<numbers.length; i++){
+        let num1 = numbers[i]
+        for(let j=i+1; j<numbers.length; j++) {
+            let num2 = numbers[j]
+            if (num1 + num2 === 0) {
+                console.log([num1,num2])
+            }
+        }
+    }
+}
+// ###################################################
