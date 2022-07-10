c011a3d122d79c95b9a7cdb2223b3c7edfaddfaf
Justin Kilburn <thatmoonman@gmail.com>
2022-07-10 15:29:38 -0400

    arraays

--- aAOpenWork/arrayadd.js
+++ aAOpenWork/arrayadd.js
@@ -345,3 +345,26 @@ function summationSequence(start, length) {
 
 console.log(summationSequence(3, 4)); // [3, 6, 21, 231]
 console.log(summationSequence(5, 3)); // [5, 15, 120]
+
+/////////////////////////////////////////////////////////
+function doubleSequence(base, length) {
+    let array = []
+    
+    if (length === 0) {
+        return array
+    } else {
+        array.push(base)
+    
+        for (let i = 1; i < length; i++) {
+            array.push(array[i-1] * 2)
+        }
+    }
+    return array
+    
+}
+
+
+console.log(doubleSequence(7, 3));  // [7, 14, 28]
+console.log(doubleSequence(3, 5));  // [3, 6, 12, 24, 48]
+console.log(doubleSequence(5, 3));  // [5, 10, 20]
+console.log(doubleSequence(5, 4));  // [5, 10, 20, 40]
